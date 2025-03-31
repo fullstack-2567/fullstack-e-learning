1c46d73bc4d0993ca9c633bcf5c44c3ff33e9834
nattaphat <eakacit@gmail.com>
2025-03-31 14:14:41 +0700

    uopdated openapi client

---
+++ src/utils/api-client.ts
@@ -0,0 +1,13 @@
+import OpenAPIClientAxios from 'openapi-client-axios'
+import { Client } from './?????/backend-openapi' // <- edit this to point to generated declaration file
+
+const API_URL = import.meta.env.VITE_END_POINT
+
+const api = new OpenAPIClientAxios({
+  definition: `${API_URL}/api-json`,
+  withServer: { url: API_URL }
+})
+api.init()
+
+const client = await api.getClient<Client>()
+export default client
