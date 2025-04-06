8731cb0e4fa3d9255e04a9de72c76dad211e0a32
nattaphat <eakacit@gmail.com>
2025-04-07 01:11:51 +0700

    initial my own branch

---
+++ src/api/ProjectApi.ts
@@ -0,0 +1,33 @@
+
+import { Project, SubmitProjectDto } from "@/utils/backend-openapi";
+import axios from "axios";
+
+export const createProject = async (projectData: SubmitProjectDto) => {
+    try {
+        const response = await axios.post<Project>(`${process.env.VITE_END_POINT}/projects/submit`, projectData);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating project:", error);
+        throw error;
+    }
+}
+
+export const getAllProject = async () => {
+    try {
+        const response = await axios.get<Project[]>(`${process.env.VITE_END_POINT}/projects`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching projects:", error);
+        throw error;
+    }
+}
+
+export const updateProjectStatus = async (projectId: number)=> {
+    try {
+        const response = await axios.post<Project>(`${process.env.VITE_END_POINT}/projects/${projectId}/update-status`, {});
+        return response.data;
+    } catch (error) {
+        console.error("Error updating project status:", error);
+        throw error;
+    }
+}
--- src/pages/learner-projectSubmiter/SubmitProject.tsx
+++ src/pages/learner-projectSubmiter/SubmitProject.tsx
@@ -1,6 +1,75 @@
 import LearningNavbar from "@/components/learner-projectSubmiter/LearnerNavbar";
+import { SubmitProjectDto, User } from "@/utils/backend-openapi";
+
+import { useFormik } from 'formik';
+import { useLocation } from "react-router-dom";
+import * as yup from 'yup';
 
 export default function SubmitProject() {
+    const formikForCreateProject = useFormik<SubmitProjectDto>({
+        initialValues: {
+            projectThaiName: "",
+            projectEngName: "",
+            projectSummary: "",
+            startDate: "",
+            endDate: "",
+            sdgType: "SDG1",
+            projectDescriptionFile: "",
+            projectType: "energy_and_environment",
+            parentProjectID: undefined,
+            userInfo: {},
+        },
+        validationSchema: yup.object({
+            projectThaiName: yup.string().required("Required").min(1, "Too short").max(255, "Too long"),
+            projectEngName: yup.string().required("Required").min(1, "Too short").max(255, "Too long"),
+            projectSummary: yup.string().required("Required").min(1, "Too short").max(1000, "Too long"),
+            startDate: yup.date().required("Required"),
+            endDate: yup.date().required("Required"),
+            sdgType: yup.string().required("Required"),
+            projectDescriptionFile: yup.mixed().required("Required"),
+            projectType: yup.string().required("Required"),
+            parentProjectID: yup.string(),
+        }),
+        onSubmit: (values) => {
+            // Handle form submission here
+            console.log("Form values:", values);
+        },
+    });
+
+    const formikForCreatingUser = useFormik<User>({
+        initialValues: {
+            userId: "",
+            email: "",
+            createdDT: "",
+            updatedAT: "",
+            role: "user",
+            sex: "male",
+            firstName: "",
+            lastName: "",
+            birthDate: "",
+            prefix: "master",
+            education: "bachelor",
+            tel: "",
+        },
+
+        validationSchema: yup.object({
+            email: yup.string().email("Invalid email").required("Required"),
+            role: yup.string().required("Required"),
+            sex: yup.string().required("Required"),
+            firstName: yup.string().required("Required").min(1, "Too short").max(100, "Too long"),
+            lastName: yup.string().required("Required").min(1, "Too short").max(100, "Too long"),
+            birthDate: yup.date().required("Required").max("", "Date must be in the past"),
+            prefix: yup.string().required("Required"),
+            education: yup.string().required("Required"),
+            tel: yup.string().required("Required").matches(/^[0-9]+$/, "Only numbers are allowed")
+        }),
+        onSubmit: (values) => {
+            // Handle form submission here
+            console.log("Form values:", values);
+        },
+    });
+
+    
     return (
         <div>
             <LearningNavbar />
