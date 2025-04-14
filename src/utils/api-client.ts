e70db4102c74ffd85e95de3effceb92a5a1b9eab
RewMan007 <pythonman.rew@gmail.com>
2025-04-14 16:54:56 +0700

    change base url because Mod chnage backend endpoint

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -17,7 +17,7 @@ const openApiclient = await api.getClient<Client>();
 
 // TO DO: Get rid of this after convert all api fetching to use openapiClient
 const client = axios.create({
-  baseURL: `${BACKEND_URL}/api`,
+  baseURL: `${BACKEND_URL}`,
   withCredentials: true,
 });
 
